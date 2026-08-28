405d736fa8dea45036808400bcfeaad8a1a6151a
EDWIN CHULLIYIL THOMAS <81749236+edwin6894@users.noreply.github.com>
2026-08-28 14:08:34 +0400

    Enhance preload.js with update functionality
    Added update-related functions and events to contextBridge.

--- preload.js
+++ preload.js
@@ -2,5 +2,17 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('detectX', {
     getSystemInfo: () => ipcRenderer.invoke('get-system-info'),
-    openSystemInfo: () => ipcRenderer.invoke('open-system-info')
+    openSystemInfo: () => ipcRenderer.invoke('open-system-info'),
+    
+    // Update functions
+    checkForUpdates: () => ipcRenderer.invoke('check-for-updates'),
+    downloadUpdate: () => ipcRenderer.invoke('download-update'),
+    installUpdate: () => ipcRenderer.invoke('install-update'),
+    
+    // Update events
+    onUpdateAvailable: (callback) => ipcRenderer.on('update-available', callback),
+    onUpdateNotAvailable: (callback) => ipcRenderer.on('update-not-available', callback),
+    onDownloadProgress: (callback) => ipcRenderer.on('download-progress', callback),
+    onUpdateDownloaded: (callback) => ipcRenderer.on('update-downloaded', callback),
+    onUpdateError: (callback) => ipcRenderer.on('update-error', callback)
 });
